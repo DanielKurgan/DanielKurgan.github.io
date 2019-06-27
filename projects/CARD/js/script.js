31a57a79165b16744d2f73d82f27b44b2815c500
DK.Company <37976060+DanielKurgan@users.noreply.github.com>
2019-06-28 02:40:34 +1000

    Upgrade animation

--- projects/CARD/js/script.js
+++ projects/CARD/js/script.js
@@ -2,14 +2,33 @@ function paynow(){
 	document.getElementById("hide-content").style.display = "none";
 	document.getElementById("hide-cvv").style.display = "none";
 	document.getElementById("hide-logos").style.display = "none";
-	document.getElementById("button-cancel").style.display = "block";
-	document.getElementById("hide-block").style.display = "flex";
+	let loader = document.getElementById("loader");
+	 loader.style.display = 'flex'; // показываем .loader
+	  setTimeout(function() {
+	    loader.style.display = 'none'; // скрываем .loader
+	  }, 2000);
+	 setTimeout(function() {
+		document.getElementById("button-cancel").style.display = "block";
+		document.getElementById("hide-block").style.display = "flex";
+		document.getElementById("firststap").style.display = "none";
+		document.getElementById("secondstap").style.display = "flex";
+	 }, 2000)
+
 }
 
 function goback(){
-	document.getElementById("hide-content").style.display = "block";
-	document.getElementById("hide-cvv").style.display = "block";
-	document.getElementById("hide-logos").style.display = "block";
 	document.getElementById("button-cancel").style.display = "none";
 	document.getElementById("hide-block").style.display = "none";
+	document.getElementById("firststap").style.display = "flex";
+	document.getElementById("secondstap").style.display = "none";
+	let loader = document.getElementById("loader");
+	 loader.style.display = 'flex'; // показываем .loader
+	  setTimeout(function() {
+	    loader.style.display = 'none'; // скрываем .loader
+	  }, 2000);
+	 setTimeout(function() {
+		document.getElementById("hide-content").style.display = "block";
+		document.getElementById("hide-cvv").style.display = "block";
+		document.getElementById("hide-logos").style.display = "block";
+	 }, 2000)
 }
