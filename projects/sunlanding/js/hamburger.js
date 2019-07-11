a0bd4ba86560f9f10b9161b53f34f4a4b484697e
DK.Company <37976060+DanielKurgan@users.noreply.github.com>
2019-07-12 00:15:01 +1000

    Upaapaapapa

---
+++ projects/sunlanding/js/hamburger.js
@@ -0,0 +1,7 @@
+function openHamburger() {
+	document.getElementById("hamburger-menu").classList.add("nav__mobile--shown");
+}
+function closeHamburger() {
+	document.getElementById("hamburger-menu").classList.remove("nav__mobile--shown");
+}
+
