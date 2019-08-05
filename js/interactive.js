3a2c18568eb07b0bec3fe89998c6338e8c33d37c
DK.Company <37976060+DanielKurgan@users.noreply.github.com>
2019-08-05 18:35:46 +1000

    Upgrade

--- js/interactive.js
+++ js/interactive.js
@@ -1,5 +1,5 @@
 $('.mainbag').viewScroller({
-	useScrollbar: false, // Use scrollbar to change views
+	useScrollbar: true, // Use scrollbar to change views
 	changeWhenAnim: false, // Change views when they are changing
 	loopMainViews: true, // Change horizontal views (mainviews) in loop mode
 	loopSubViews: true	// Change vertical views (subviews) in loop mode
