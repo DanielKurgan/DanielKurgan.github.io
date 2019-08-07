daa2deae03ce269db18919d7993f6bd2ae5e9a83
DK.Company <37976060+DanielKurgan@users.noreply.github.com>
2019-08-08 08:52:35 +1000

    Upgrade

--- portfolio/js/interactive.js
+++ portfolio/js/interactive.js
@@ -1,10 +1,3 @@
-$('.mainbag').viewScroller({
-	useScrollbar: true, // Use scrollbar to change views
-	changeWhenAnim: false, // Change views when they are changing
-	loopMainViews: true, // Change horizontal views (mainviews) in loop mode
-	loopSubViews: true	// Change vertical views (subviews) in loop mode
-});
-
 function bosshoverOver() {
 	$(".boss-image.hover").css("display", "block");
 }
@@ -12,3 +5,85 @@ function bosshoverOut() {
 	$(".boss-image.not-hover").css("display", "block");
 	$(".boss-image.hover").css("display", "none");
 }
+
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+});
+
+$(document).ready(function() {
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(500).fadeOut("slow");
+
+});
+
+// $(document).ready(function(){
+
+// 	});
+
+$('.skillbar').skillBars({
+
+  // number start
+  from: 0,       
+
+  // number end 
+  to: false,      
+
+  // animation speed
+  speed: 1500,   
+
+  // how often the element should be up<a href="https://www.jqueryscript.net/time-clock/">date</a>d
+  interval: 500,    
+
+  // the number of decimal places to show
+  decimals: 0,      
+
+  // callback method for every time the element is updated,
+  onUpdate: null,   
+
+  // callback method for when the element finishes updating
+  onComplete: null,   
+
+  // CSS classes
+  classes:{
+    skillBarBar : '.skillbar-bar',
+    skillBarPercent : '.skill-bar-percent',
+  }
+  
+});
