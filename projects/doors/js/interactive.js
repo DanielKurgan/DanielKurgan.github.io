8733dde31c9ca8e5bc5240d809d419ed7607f5bd
Daniel <danielkvinov@mail.ru>
2019-09-02 14:17:19 +1000

    Pravki -__-

--- projects/doors/js/interactive.js
+++ projects/doors/js/interactive.js
@@ -7,10 +7,24 @@ $('.main-slider').slick({
     dots: true,
 });
 
-var $hamburger = $(".hamburger");
-$hamburger.on("click", function(e) {
-  $hamburger.toggleClass("is-active");
-  $(".hamburger-box").toggleClass("hamburger-height");
+var $hamburger1 = $(".header-top__line .container .hamburger");
+$hamburger1.on("click", function(e) {
+  $hamburger1.toggleClass("is-active");
+  $(".header-top__line .container .hamburger-box").toggleClass("hamburger-height");
+  // Do something else, like open/close menu
+});
+
+var $hamburger2 = $(".header-top__menu .hamburger");
+$hamburger2.on("click", function(e) {
+  $hamburger2.toggleClass("is-active");
+  $(".header-top__menu .hamburger-box").toggleClass("hamburger-height");
+  // Do something else, like open/close menu
+});
+
+var $hamburger3 = $(".store .hamburger");
+$hamburger3.on("click", function(e) {
+  $hamburger3.toggleClass("is-active");
+  $(".store .hamburger-box").toggleClass("hamburger-height");
   // Do something else, like open/close menu
 });
 
@@ -88,6 +102,81 @@ $('.makers-slider').slick({
       ]
 });
 
-$(".hamburger").click(function(){
+$(".header-top__line .container .hamburger").click(function(){
+    $('.header-top__anchors').toggleClass('menu-visible');
+});
+
+$(".header-top__menu .hamburger").click(function(){
+    $('.header-top__menu nav').toggleClass('menu-visible');
+});
+
+$(".store .hamburger").click(function(){
     $('.store-menu__content').toggleClass('menu-visible');
 });
+
+
+
+$(".bunch-card1").hover(
+  function() {
+  $(".bunch-name1").css("background-color", "#e31e24");
+  },
+  function(){
+  $(".bunch-name1").css("background-color", "#191919");
+});
+
+$(".bunch-card2").hover(
+  function() {
+  $(".bunch-name2").css("background-color", "#e31e24");
+  },
+  function(){
+  $(".bunch-name2").css("background-color", "#191919");
+});
+
+$(".bunch-card3").hover(
+  function() {
+  $(".bunch-name3").css("background-color", "#e31e24");
+  },
+  function(){
+  $(".bunch-name3").css("background-color", "#191919");
+});
+
+$(".bunch-card4").hover(
+  function() {
+  $(".bunch-name4").css("background-color", "#e31e24");
+  },
+  function(){
+  $(".bunch-name4").css("background-color", "#191919");
+});
+
+$(".bunch-card5").hover(
+  function() {
+  $(".bunch-name5").css("background-color", "#e31e24");
+  },
+  function(){
+  $(".bunch-name5").css("background-color", "#191919");
+});
+
+$(".bunch-card6").hover(
+  function() {
+  $(".bunch-name6").css("background-color", "#e31e24");
+  },
+  function(){
+  $(".bunch-name6").css("background-color", "#191919");
+});
+
+$(".bunch-card7").hover(
+  function() {
+  $(".bunch-name7").css("background-color", "#e31e24");
+  },
+  function(){
+  $(".bunch-name7").css("background-color", "#191919");
+});
+
+$(".bunch-card8").hover(
+  function() {
+  $(".bunch-name8").css("background-color", "#e31e24");
+  },
+  function(){
+  $(".bunch-name8").css("background-color", "#191919");
+});
+
