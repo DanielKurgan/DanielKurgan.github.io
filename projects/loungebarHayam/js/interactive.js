faa011014bd8f48b36831dbb988ae0a3b1cdf34f
Daniel <danielkvinov@mail.ru>
2019-09-29 00:28:12 +1000

    1

--- projects/loungebarHayam/js/interactive.js
+++ projects/loungebarHayam/js/interactive.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 		dots: false,
 		infinite: true,
 		slidesToShow: 3,
-		slidesToScroll: 1,
+		slidesToScroll: 3,
 		variableWidth: true,
 		autoplay: true,
 		autoplaySpeed: 2000,
