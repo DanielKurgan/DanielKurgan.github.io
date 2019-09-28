60c2e747d93f24f1e9729400f6add327cba7e4cb
Daniel <danielkvinov@mail.ru>
2019-09-28 18:32:34 +1000

    Desktop version

--- projects/loungebarHayam/js/interactive.js
+++ projects/loungebarHayam/js/interactive.js
@@ -1,8 +1,34 @@
-$('.photo-slider').slick({
-	arrows: false,
-	dots: false,
-  infinite: false,
-  slidesToShow: 3.1,
-  slidesToScroll: 1,
-  variableWidth: true,
+$(document).ready(function() {
+	// FANCYBOX
+
+		$(".photo-slider__slide").fancybox({
+			minWidth: 530,
+			minHeight: 629,
+			autoScale: true,
+		});
+
+	// FANCYBOX END
+
+	// SLIDER
+	$('.photo-slider').slick({
+		arrows: false,
+		dots: false,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		variableWidth: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+	});
+	// SLIDER END
+
+
+
+
+
+
+
+
+
+
 });
---
+++ projects/loungebarHayam/js/yandexmap.js
@@ -0,0 +1,34 @@
+// MAP
+ymaps.ready(init);
+function init(){  
+ var myMap = new ymaps.Map("map", {
+     center: [55.758225068983045,37.62521699999994],
+     zoom: 17,
+     controls: []
+ }, {
+     searchControlProvider: 'yandex#search'
+ }),
+ myPlacemark = new ymaps.Placemark([55.758225068983045,37.62521699999994], {
+// Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
+balloonContentHeader: "Балун метки",
+balloonContentBody: "Содержимое <em>балуна</em> метки",
+balloonContentFooter: "Подвал",
+hintContent: "Хинт метки"
+
+}, {
+// Опции.
+// Необходимо указать данный тип макета.
+iconLayout: 'default#image',
+// Своё изображение иконки метки.
+iconImageHref: './img/map-shape.png',
+// Размеры метки.
+iconImageSize: [60, 82],
+// Смещение левого верхнего угла иконки относительно
+// её "ножки" (точки привязки).
+iconImageOffset: [-5, -38]
+});
+
+ myMap.geoObjects.add(myPlacemark);
+}
+
+// MAP END
