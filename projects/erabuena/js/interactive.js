92fd8844081578997ae89f48a08fd1705a47259f
Daniel <danielkvinov@mail.ru>
2020-02-19 18:13:55 +1000

    1

--- projects/erabuena/js/interactive.js
+++ projects/erabuena/js/interactive.js
@@ -364,3 +364,13 @@ $(document).ready(function(){
     jQuery(document).ready(function($) {
       $('.catalog-items__item-content').matchHeight();
   });
+
+$(function($){
+  $(document).mouseup(function (e){ // событие клика по веб-документу
+    var div = $(".product-popup__container-block"); // тут указываем ID элемента
+    if (!div.is(e.target) // если клик был не по нашему блоку
+        && div.has(e.target).length === 0) { // и не по его дочерним элементам
+        $(".product-popup").fadeOut();
+    }
+  });
+});
