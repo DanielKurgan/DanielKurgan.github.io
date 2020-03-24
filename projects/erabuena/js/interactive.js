d61f05f9d66b71d3141af680d595c4bffeb66e35
Daniel <danielkvinov@mail.ru>
2020-03-25 01:33:49 +1000

    1

--- projects/erabuena/js/interactive.js
+++ projects/erabuena/js/interactive.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
   // Выравниваем по высоте
   function matchHeight(){
    $('.comparison-slider-mobile').matchHeight();
-   // $('.catalog-items__item-content').matchHeight();
+   $('.catalog-items__item-content').matchHeight();
+   $('.catalog-items__item').matchHeight();
    $('.catalog-items__item-inner').matchHeight();
    $('.catalog-items__item-container').matchHeight();
    $('.item-kit__block-content').matchHeight();
